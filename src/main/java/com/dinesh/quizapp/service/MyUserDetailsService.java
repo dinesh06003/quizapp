1538f38155b1c8783e8e9388d13ee093f2af2c38
Sai Dinesh Reddy Gunnala <gunnalasaidinesh@gmail.com>
2024-11-19 16:10:21 -0600

    added security config

---
+++ src/main/java/com/dinesh/quizapp/service/MyUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.dinesh.quizapp.service;
+
+import com.dinesh.quizapp.model.User;
+import com.dinesh.quizapp.model.UserPrincipal;
+import com.dinesh.quizapp.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username);
+        if(user == null){
+            System.out.println("user not found: MyUserDetailService");
+        }
+        return new UserPrincipal(user);
+    }
+}
--- src/main/java/com/dinesh/quizapp/service/UserService.java
+++ src/main/java/com/dinesh/quizapp/service/UserService.java
@@ -3,6 +3,7 @@ package com.dinesh.quizapp.service;
 import com.dinesh.quizapp.model.User;
 import com.dinesh.quizapp.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,21 +13,13 @@ public class UserService {
     @Autowired
     private UserRepo userRepo;
 
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
     public String saveUser(User user) {
+        user.setPassword(encoder.encode(user.getPassword()));
          userRepo.save(user);
          String result = user.toString();
         System.out.println(result);
          return "successfully registered user details in the database with user " + result;
     }
 
-    public boolean checkUser(User user) {
-        User findUser = userRepo.findByUsername(user.getUsername());
-        if(findUser.getUsername().equals(user.getUsername()) && findUser.getPassword().equals(user.getPassword())){
-            return true;
-        }
-        else {
-            return false;
-        }
-
-    }
 }
